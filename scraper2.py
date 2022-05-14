69b161e9f23041f62efb0bfdaa8e7b5106ffbd28
realdino-jpj <78307958+realdino-jpj@users.noreply.github.com>
2022-05-14 17:40:11 +0530

    Add files via upload

--- scraper2.py
+++ scraper2.py
@@ -2,9 +2,38 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 import csv
+import pandas as pd
 import requests
 
-START_URL = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
+dwarf_stars_url = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
 browser = webdriver.Chrome("chromedriver.exe")
-browser.get(START_URL)
-time.sleep(10)
+
+page = requests.get(dwarf_stars_url)
+print(page)
+
+soup = BeautifulSoup(page.text,"html.parser")
+star_table = soup.find_all('table')
+table_rows = star_table[7].find_all('tr')
+
+temp_lists = []
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_lists.append(row)
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+
+for i in range(1,len(temp_lists)):
+    Star_names.append(temp_lists[i][1])
+    Distance.append(temp_lists[i][3])
+    Mass.append(temp_lists[i][5])
+    Radius.append(temp_lists[i][6])
+    
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius)),columns=['Star_name','Distance','Mass','Radius','Luminosity'])
+print(df2)
+
+df2.to_csv('dwarf_stars.csv')
